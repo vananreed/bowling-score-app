3c29b1c9e070d30f9b5f22bd6874ac3155e22fb0
Reed Van Anrooy <reed.vananrooy@gmail.com>
2019-06-21 16:40:07 -0700

    Updates

---
+++ precache-manifest.0d2c30efea767236880e18fe77764c77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb65729c58e19d6d4b477cf596e8ab2",
+    "url": "/bowling-score-app/index.html"
+  },
+  {
+    "revision": "37dcb33a1e620700bbf6",
+    "url": "/bowling-score-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/bowling-score-app/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "37dcb33a1e620700bbf6",
+    "url": "/bowling-score-app/static/js/main.be0b065c.chunk.js"
+  },
+  {
+    "revision": "f09c4d57696dfd4e7c6b",
+    "url": "/bowling-score-app/static/js/runtime~main.f5160d95.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bowling-score-app/precache-manifest.0d2c30efea767236880e18fe77764c77.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bowling-score-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
